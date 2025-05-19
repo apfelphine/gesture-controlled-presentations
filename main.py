bbf3327194082fd7d7fd2f1422011ecb20f21218
Sophie <sharren@stud.hs-bremen.de>
2025-05-19 10:57:47 +0200

    Push gesture recognition model & Update App skeleton

--- main.py
+++ main.py
@@ -7,58 +7,69 @@ import cv2
 from action_controller.action_controller import ActionController
 
 BaseOptions = mp.tasks.BaseOptions
-GestureRecognizer = mp.tasks.vision.GestureRecognizer
-GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
-GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
-VisionRunningMode = mp.tasks.vision.RunningMode
+vision = mp.tasks.vision
 
 video = cv2.VideoCapture(0)
 
-gesture_recognition = GestureRecognizerOptions(
+gesture_recognition_options = vision.GestureRecognizerOptions(
     base_options=BaseOptions(
         model_asset_path='gesture_recognition/gesture_recognizer_model/gesture_recognizer.task'
     ),
-    running_mode=VisionRunningMode.VIDEO,
+    running_mode=vision.RunningMode.VIDEO,
     num_hands=2
 )
+pose_landmark_options = vision.PoseLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path='../tasks/pose_landmarker_heavy.task'),
+    running_mode=vision.RunningMode.VIDEO,
+    output_segmentation_masks=False,
+    num_poses=1,
+)
+
 start = time.perf_counter()
 
 action_controller = ActionController()
 
 # todo: richtiges window fokussieren
 
-with GestureRecognizer.create_from_options(gesture_recognition) as recognizer:
-    while video.isOpened():
-        ret, frame = video.read()
-        if not ret:
-            break
+with vision.GestureRecognizer.create_from_options(gesture_recognition_options) as gesture_recognizer:
+    with vision.PoseLandmarker.create_from_options(pose_landmark_options) as pose_landmark_detection:
+        while video.isOpened():
+            ret, frame = video.read()
+            if not ret:
+                break
 
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
 
-        gesture_detection_result = recognizer.recognize_for_video(mp_image, int((time.perf_counter() - start) * 1000.0))
-        # gesture_detection_result.hand_landmarks
-        # gesture_detection_result.handedness
-        # gesture_detection_result.gestures
+            timestamp = int((time.perf_counter() - start) * 1000.0)
 
-        action_result = action_controller(gesture_detection_result)
-        # action_result.gesture
-        # action_result.action (e.g. "point", "prev", "next")
-        # action_result.triggered
+            gesture_detection_result = gesture_recognizer.recognize_for_video(mp_image, timestamp)
+            # gesture_detection_result.hand_landmarks
+            # gesture_detection_result.handedness
+            # gesture_detection_result.gestures
 
-        if action_result.action is not None:
-            if action_result.triggered:
-                if action_result.action == "prev":
-                    pyautogui.press(pyautogui.LEFT)
-                elif action_result.action == "next":
-                    pyautogui.press(pyautogui.RIGHT)
+            action_result = action_controller(gesture_detection_result)
+            # action_result.gesture
+            # action_result.action (e.g. "point", "prev", "next")
+            # action_result.triggered
 
-        # todo: pointing target detection aufrufen
-        # todo: overlay
+            pose_result = pose_landmark_detection.detect_for_video(mp_image, timestamp)
+            # pose_result.pose_landmarks
 
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
+            # todo: pointing target detection aufrufen
+            # todo: overlay
+
+            # Convert Action to Keypress
+            if action_result.action is not None:
+                if action_result.triggered:
+                    if action_result.action == "prev":
+                        pyautogui.press(pyautogui.LEFT)
+                    elif action_result.action == "next":
+                        pyautogui.press(pyautogui.RIGHT)
+
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
 
 video.release()
 cv2.destroyAllWindows()
